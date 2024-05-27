6a0f59074184601dae81d3105464db974743f2c8
Gerardo Ordoñez <gerardo.e.ordonez.f@gmail.com>
2024-05-26 18:18:54 -0600

    .

--- gui.py
+++ gui.py
@@ -3,7 +3,10 @@ import PySimpleGUI as sg
 
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter a ToDo",)
+add_button = sg.Button("Add")
+window = sg.Window("My ToDO App", layout=[[label], [input_box, add_button ]])
 
-window = sg.Window("My ToDO App", layout=[[label], [input_box, ]])
 window.read()
+
 window.close()
+
