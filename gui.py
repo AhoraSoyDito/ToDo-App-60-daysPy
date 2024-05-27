e842c9e671ebaf1faf2fc9558eee15b9bd7dcf75
Gerardo Ordoñez <gerardo.e.ordonez.f@gmail.com>
2024-05-26 19:17:15 -0600

    Adding Editn Button

--- gui.py
+++ gui.py
@@ -2,10 +2,30 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a To-Do")
+
 input_box = sg.InputText(tooltip="Enter a ToDo", key="todo")
+
 add_button = sg.Button("Add")
+
+edit_button = sg.Button("Edit")
+
+
+list_box = sg.Listbox(values = functions.get_todos(),
+                        key= "todos",
+                        enable_events = "True",
+                        size=[45,10])
+
+
+
+
+
 window = sg.Window("My ToDO App", 
-                    layout=[[label], [input_box, add_button ]], 
+                    layout=
+                    [
+                        [label],
+                        [input_box, add_button],
+                        [list_box, edit_button]
+                    ], 
                     font=('Times New Roman', 10) )
 
 
@@ -21,6 +41,24 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["todos"].update(values = todos)
+        
+        case "Edit":
+            todo_to_edit = values['todos'][0]   
+            new_todo = values['todo'] + "\n"
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            
+            window["todos"].update(values = todos)
+        
+        case "todos":
+            window["todo"].update(value = values['todos'][0])
+        
+        
+        
         case sg.WIN_CLOSED:
             break
     
